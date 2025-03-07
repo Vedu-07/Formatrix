2dd7db8af250c0e91c94cc28142e89fb0bc94e60
Vedu-07 <vedantswami02@gmail.com>
2025-03-07 18:22:33 +0530

    Form Stats Ui & Action Added

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function Dashboard() {
     return (
-      <div>Dashboard</div>
+      <div className="w-4 pt-8">
+        <div className="w-full max-w-6xl mx-auto pt-1 px-2 md:px-0">
+          {/* Form Stats */}
+            <section className="stats-section w-full">
+                <div className="flex items-center justify-between py-5">
+                  <h1 className="text-3xl font-semibold tracking-tighter">Dashboard</h1>
+                  <Button>Create Form</Button>
+                </div>
+            </section>
+        </div>
+      </div>
     );
   }
