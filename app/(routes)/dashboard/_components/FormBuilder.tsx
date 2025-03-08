f5be695ff0251d55e3e8e4cfdd8b1b16760ab1fb
Vedu-07 <vedantswami02@gmail.com>
2025-03-08 13:56:31 +0530

    Form Builder Sidebar Started

--- app/(routes)/dashboard/_components/FormBuilder.tsx
+++ app/(routes)/dashboard/_components/FormBuilder.tsx
@@ -1,7 +1,28 @@
-"use client"
+"use client";
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { useState } from "react";
+import Builder from "./Builder";
 
 const FormBuilder = () => {
-    return <div>FormBuilder</div>
-}
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  return (
+    <div>
+      <SidebarProvider
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+        className="h-[calc(100vh_-_64px)] "
+        style={
+          {
+            "--sidebar-width": "300px",
+            "--sidbar-height": "40px",
+          } as React.CSSProperties
+        }
+      >
+        <Builder {...{ isSidebarOpen }} />
+      </SidebarProvider>
+    </div>
+  );
+};
 
 export default FormBuilder;
