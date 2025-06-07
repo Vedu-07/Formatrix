91250492bde84753540d8238f265da4652ce2b78
Vedu-07 <vedantswami02@gmail.com>
2025-06-07 23:04:51 +0530

    Config changes

--- app/(routes)/dashboard/form/builder/[formId]/page.tsx
+++ app/(routes)/dashboard/form/builder/[formId]/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import FormBuilder from "../../../_components/FormBuilder";
 import BuilderContextProvider from "@/context/builder-provider";
 
+
 const Builder = () => {
   return (
     <BuilderContextProvider>
--- app/api/fetchFormById/route.ts
+++ app/api/fetchFormById/route.ts
@@ -57,6 +57,8 @@
 //   }
 // }
 
+
+
 // app/api/fetchFormById/route.ts
 console.log("NODE_ENV:", process.env.NODE_ENV);
 console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
@@ -67,32 +69,6 @@ export const fetchCache = "force-no-store";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 import { fetchFormByIdForUser } from "@/lib/fetchform";
-
-// export async function GET(req: Request) {
-//   try {
-//     const session = getKindeServerSession();
-//     const user = await session.getUser();
-
-//     if (!user) {
-//       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-//     }
-
-//     const { searchParams } = new URL(req.url);
-//     const formId = searchParams.get("formId");
-//     if (!formId) {
-//       return NextResponse.json({ error: "formId is required" }, { status: 400 });
-//     }
-
-//     const form = await fetchFormByIdForUser(user.id, formId);
-//     if (!form) {
-//       return NextResponse.json({ error: "Form not found" }, { status: 404 });
-//     }
-
-//     return NextResponse.json({ data: { success: true, form } });
-//   } catch (error: any) {
-//     return NextResponse.json({ error: error?.message || "Internal Server Error" }, { status: 500 });
-//   }
-// }
 
 export async function GET(req: Request) {
   try {
